52834eb3ee2c80f6efbd8e2f38e95555186dea32
Bart Custers <bart_custers@hotmail.com>
2025-11-10 20:33:24 +0100

    fix2

--- utils/prompt_library.py
+++ utils/prompt_library.py
@@ -32,6 +32,16 @@ PROMPTS = {
     - No explanations or comments outside the code block.
     - Do NOT import any modules.
     - Do NOT read/write files.
+    - Make sure the python code always contains these elements: {
+    "X_train": ...,
+    "X_test": ...,
+    "y_train": ...,
+    "y_test": ...,
+    "exposure_train": ...,
+    "exposure_test": ...,
+    "feature_names": [...],
+    "actions_log": [...]
+    }
     - Wrap the code in triple backticks like this:
 
     ```python code here```
