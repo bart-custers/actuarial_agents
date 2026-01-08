9c961f802de43e984248592fecfc57cd4408d623
Bart Custers <bart_custers@hotmail.com>
2026-01-08 12:42:43 +0100

    fix2

--- utils/data_cleaning.py
+++ utils/data_cleaning.py
@@ -28,11 +28,5 @@ class DataCleaning:
         area_mapping = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6}
         data['Area'] = data['Area'].map(area_mapping)
         self.actions_log.append("Mapped Area categories to numeric scale (A→1,...,F→6).")
-
-        # --- Step 3: Drop missing values ---
-        before = len(data)
-        #data = data.dropna()
-        after = len(data)
-        self.actions_log.append(f"Dropped {before - after} rows with missing values.")
         
         return data
