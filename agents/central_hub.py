4e945183c0e56879d710fa4b4f2688f9088711cc
Bart Custers <bart_custers@hotmail.com>
2026-02-10 09:41:15 +0100

    test6

--- agents/central_hub.py
+++ agents/central_hub.py
@@ -66,7 +66,7 @@ class CentralHub:
         # INITIAL STATE
         # ------------------------------------------------------------------
         # current_metadata = {"dataset_path": "data/raw/freMTPL2freq.csv"}
-        current_metadata = {"dataset_path": "data/raw/T5_missing_column.csv"}
+        current_metadata = {"dataset_path": "data/raw/T6_data_increase.csv"}
         phase = "dataprep"
         iteration = 1
         continue_workflow = True
--- agents/data_prep_agent.py
+++ agents/data_prep_agent.py
@@ -219,7 +219,7 @@ class DataPrepAgent(BaseAgent):
         # --------------------
         # Load dataset
         # --------------------
-        dataset_path = message.metadata.get("dataset_path", "data/raw/T5_missing_column.csv")
+        dataset_path = message.metadata.get("dataset_path", "data/raw/T6_data_increase.csv")
         try:
             df = pd.read_csv(dataset_path)
         except Exception as e:
