6cf4f56052d2c43700fc2c2c909ea1794cb98b04
Bart Custers <bart_custers@hotmail.com>
2026-02-10 16:43:56 +0100

    test11

--- agents/central_hub.py
+++ agents/central_hub.py
@@ -66,7 +66,7 @@ class CentralHub:
         # INITIAL STATE
         # ------------------------------------------------------------------
         # current_metadata = {"dataset_path": "data/raw/freMTPL2freq.csv"}
-        current_metadata = {"dataset_path": "data/raw/T10_feature_noise.csv"}
+        current_metadata = {"dataset_path": "data/raw/T11_label_noise.csv"}
         phase = "dataprep"
         iteration = 1
         continue_workflow = True
