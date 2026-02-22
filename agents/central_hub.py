0196fa7a2e1311338576c0a70ba1e42a92a2a5cf
Bart Custers <bart_custers@hotmail.com>
2026-02-22 20:38:39 +0100

    test17

--- agents/central_hub.py
+++ agents/central_hub.py
@@ -66,7 +66,7 @@ class CentralHub:
         # INITIAL STATE
         # ------------------------------------------------------------------
         #current_metadata = {"dataset_path": "data/raw/freMTPL2freq.csv"}
-        current_metadata = {"dataset_path": "data/raw/T16_counterfactual.csv"}
+        current_metadata = {"dataset_path": "data/raw/T17_counterfactual.csv"}
         phase = "dataprep"
         iteration = 1
         continue_workflow = True
